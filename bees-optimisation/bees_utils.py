dee9db7a60020353d8b695ed88a3f1ca31aea877
adam <adam.przywieczerski@gmail.com>
2022-04-29 14:02:24 +0200

    basic bees-optimization

---
+++ bees-optimisation/bees_utils.py
@@ -0,0 +1,243 @@
+import random
+import numpy as np
+from queue import Queue
+
+NOTHING = -1
+CENTRE = 0
+HOUSE = 1
+FUN = 2
+WORK = 3
+
+HAPPINESS_TIME = 0.1  # [0, 0.2]
+
+HOUSE_COST = 25600
+FUN_COST = 12800
+WORK_COST = 19200
+COSTS = [0, HOUSE_COST, FUN_COST, WORK_COST]
+
+value = {}
+
+value[(NOTHING, NOTHING)] = (0, 0)
+value[(NOTHING, CENTRE)] = (0, 0)
+value[(NOTHING, HOUSE)] = (0, 0)
+value[(NOTHING, FUN)] = (0, 0)
+value[(NOTHING, WORK)] = (0, 0)
+
+value[(CENTRE, NOTHING)] = (0, 0)
+value[(CENTRE, CENTRE)] = (0, 0)
+value[(CENTRE, HOUSE)] = (0, 0)
+value[(CENTRE, FUN)] = (0, 1)
+value[(CENTRE, WORK)] = (1, 0)
+
+value[(HOUSE, NOTHING)] = (0, 0)
+value[(HOUSE, CENTRE)] = (0, 0)
+value[(HOUSE, HOUSE)] = (0, 0)
+value[(HOUSE, FUN)] = (0, 1)
+value[(HOUSE, WORK)] = (1, 0)
+
+value[(FUN, NOTHING)] = (0, 0)
+value[(FUN, CENTRE)] = (0, 1)
+value[(FUN, HOUSE)] = (0, 1)
+value[(FUN, FUN)] = (0, 0)
+value[(FUN, WORK)] = (0, -1)
+
+value[(WORK, NOTHING)] = (0, 0)
+value[(WORK, CENTRE)] = (1, 0)
+value[(WORK, HOUSE)] = (1, 0)
+value[(WORK, FUN)] = (0, -1)
+value[(WORK, WORK)] = (0, 0)
+
+
+def data(solution, city):
+    money = 0
+    happiness = 0
+    cost = 0
+    buildings = 0
+    for building in solution:
+        if building == NOTHING:
+            pass
+        elif building == CENTRE:
+            pass
+        elif building == HOUSE:
+            cost += HOUSE_COST
+            buildings += 1
+        elif building == FUN:
+            happiness += 1
+            cost += FUN_COST
+            buildings += 1
+        elif building == WORK:
+            happiness -= 1
+            cost += WORK_COST
+            buildings += 1
+        else:
+            print("Wrong building!")
+
+    for (x, y) in city:
+        (m, h) = value[(solution[x], solution[y])]
+        money += m
+        happiness += h
+
+    if happiness < -5:
+        happiness = -5
+    elif happiness > 5:
+        happiness = 5
+
+    return (money, happiness, buildings, cost)
+
+
+def f(money, happiness, buildings, cost):
+    if happiness < 0:
+        real_money = (1 + HAPPINESS_TIME * happiness) * money - HAPPINESS_TIME * happiness * money / 2
+    elif happiness > 0:
+        real_money = (1 - HAPPINESS_TIME * happiness) * money + HAPPINESS_TIME * happiness * money * 2
+    else:
+        real_money = money
+    return real_money
+
+
+def get_population(length, size, max_buildings=-1, graph=None, max_cost=-1):
+    population = []
+    # case 1 - no limits
+    if max_buildings == -1 and max_cost == -1:
+        for _ in range(size):
+            vector = list(np.random.choice([HOUSE, FUN, WORK], size=length))
+            vector[0] = CENTRE
+            population.append(vector)
+    # case 2,3,4
+    else:
+        if graph is None:
+            raise Exception("No graph!")
+        for _ in range(size):
+            vector = [NOTHING for _ in range(length)]
+            vector[0] = CENTRE
+            vertices_possible = set()
+            vertices_done = set()
+            for (u, v) in graph:
+                if 0 in (u, v):
+                    vertices_possible.add(v)
+                    vertices_possible.add(u)
+            vertices_done.add(0)
+            # case 2 - no max cost
+            if max_cost == -1:
+                for _ in range(max_buildings):
+                    vertex = random.choice(list(vertices_possible - vertices_done))
+                    vector[vertex] = random.choice([HOUSE, FUN, WORK])
+                    for (u, v) in graph:
+                        if vertex in (u, v):
+                            vertices_possible.add(v)
+                            vertices_possible.add(u)
+                    vertices_done.add(vertex)
+            # case 3 - no max buildings
+            elif max_buildings == -1:
+                cost = 0
+                while True:
+                    vertex = random.choice(list(vertices_possible - vertices_done))
+                    choice = random.choice([HOUSE, FUN, WORK])
+                    if cost + COSTS[choice] > max_cost:
+                        break
+                    cost += COSTS[choice]
+                    vector[vertex] = choice
+                    for (u, v) in graph:
+                        if vertex in (u, v):
+                            vertices_possible.add(v)
+                            vertices_possible.add(u)
+                    vertices_done.add(vertex)
+            # case 4 - all restraints active
+            else:
+                cost = 0
+                buildings = 0
+                while True:
+                    vertex = random.choice(list(vertices_possible - vertices_done))
+                    choice = random.choice([HOUSE, FUN, WORK])
+                    if cost + COSTS[choice] > max_cost or buildings + 1 > max_buildings:
+                        break
+                    cost += COSTS[choice]
+                    buildings += 1
+                    vector[vertex] = choice
+                    for (u, v) in graph:
+                        if vertex in (u, v):
+                            vertices_possible.add(v)
+                            vertices_possible.add(u)
+                    vertices_done.add(vertex)
+            population.append(vector)
+    return population
+
+
+def get_graph_edges(file):
+    f = open(file, "r")
+    lines = f.readlines()
+    graph = []
+    for l in lines:
+        s = l.split()
+        if s[0] == "p":
+            length = int(s[2])
+        if s[0] == "e":
+            graph.append((int(s[1])-1, int(s[2])-1))
+
+    vertices_possible = set()
+    vertices_done = set()
+    vertices_possible.add(0)
+    while vertices_possible - vertices_done:
+        vertex = random.choice(list(vertices_possible - vertices_done))
+        for (u, v) in graph:
+            if vertex in (u, v):
+                vertices_possible.add(v)
+                vertices_possible.add(u)
+        vertices_done.add(vertex)
+
+    if len(vertices_possible) != length:
+        raise Exception("The graph is not connected")
+
+    return graph, length
+
+
+class Node():
+    def __init__(self, i):
+        self.neighbours = list()
+        self.visited = False
+        self.index = i
+
+
+def get_graph(filename: str):
+    graph = []
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        graph = [Node(i) for i in range(int(lines[0].split()[2]))]
+        for line in lines[1:]:
+            u, v = line.split()[1:]
+            u, v = int(u), int(v)
+            graph[u-1].neighbours.append(graph[v-1])
+            graph[v-1].neighbours.append(graph[u-1])
+    return graph
+
+
+def check_connectivity(graph):
+    q = Queue()
+    q.put(graph[0])
+    visited = 0
+
+    for node in graph:
+        node.visited = False
+
+    while not q.empty():
+        u = q.get()
+        u.visited = True
+        visited += 1
+
+        for n in u.neighbours:
+            if not n.visited:
+                q.put(n)
+                n.visited = True
+
+    return visited == len(graph)
+
+
+
+if __name__ == "__main__":
+    G = get_graph('graphs/graph.txt')
+    print(check_connectivity(G))
+    # pop_graph = get_graph_edges('graphs/graph.txt')[0]
+    # get_population(5, 1, max_buildings=5, graph=pop_graph, max_cost=100000000)
+
+    G, number_of_vertices = get_graph_edges('graphs/graph.txt')
+    print(get_population(number_of_vertices, 5, max_buildings=number_of_vertices, max_cost=60000, graph=G))
--- bees-optimisation/main.py
+++ bees-optimisation/main.py
@@ -1,4 +1,4 @@
-from utils import *
+from bees_utils import *
 
 
 class Solution:
@@ -11,18 +11,34 @@ class Solution:
         self.cost = c
 
 
-def initial_solutions(graph) -> list[Solution]:
-    '''generuje początkową populację'''
-    pass
+def initial_solutions(graph, num_of_solutions: int, solution_length: int, max_buildings=-1, max_cost=-1) -> list[Solution]:
+    '''generuje początkowe populacje i zwraca je posortowane malejąco według ich opłacalności'''
+    result = list()
+    solutions = get_population(solution_length, num_of_solutions, max_buildings, graph, max_cost)
+    for solution in solutions:
+        result.append(Solution(solution, graph))
+    result.sort(key=lambda el: el.profitability, reverse=True)
+    return result
 
 
-def generate_solution(old_solution):
+def generate_solution(old_solution, num_of_changes=5):
     '''generuje nowe rozwiązanie na podstawie dostarczonego'''
-    pass
+    new_solution = old_solution.copy()
+    edited_nodes = [False for _ in range(len(old_solution))]
+    for _ in range(num_of_changes):
+        prob = random.random()
+        if 0 <= prob < 1:     #zmiana istniejącego budynku
+            index = random.randint(1, len(old_solution)-1)
+            new_building = random.randint(1, 3)
+            new_solution[index] = new_building
+        else:                 #dodanie nowego budynku
+            pass
+    return new_solution
+    # pass
 
 
-def first_bees(graph=None, iterations=100, elite_places=3, good_places=2, elite_bees=50, good_bees=25):
-    current_solutions = initial_solutions(graph) #lista Solutions - czyli rozwiązanie i obliczona dla niego funkcja celu, posortowane malejąco
+def first_bees(graph,  num_of_vertices, iterations=100, elite_places=3, good_places=2, elite_bees=50, good_bees=25) -> Solution:
+    current_solutions = initial_solutions(graph, elite_places+good_places, num_of_vertices) #lista Solutions - czyli rozwiązanie i obliczona dla niego funkcja celu, posortowane malejąco
     counter = 0
 
     while counter < iterations:
@@ -32,11 +48,11 @@ def first_bees(graph=None, iterations=100, elite_places=3, good_places=2, elite_
         for i, chosen_solution in enumerate(chosen_solutions):
             local_solutions = []
             for _ in range(elite_bees if i < elite_places else good_bees):
-                new_solution = generate_solution(chosen_solution)
+                new_solution = generate_solution(chosen_solution.solution)
                 solution = Solution(new_solution, graph)
                 local_solutions.append(solution)
             
-            local_solutions.sort(lambda el: el.profitability, reverse=True)
+            local_solutions.sort(key=lambda el: el.profitability, reverse=True)
             new_solutions.append(local_solutions)
         
         current_solutions = [el[0] for el in new_solutions] #pierwszy krok pętli przerzucony na koniec dla uproszczenia zapisu
@@ -45,4 +61,5 @@ def first_bees(graph=None, iterations=100, elite_places=3, good_places=2, elite_
 
 
 if __name__ == "__main__":
-    pass
+    G, number_of_vertices = get_graph_edges("graphs/city_kos.txt")
+    print(first_bees(G, number_of_vertices).profitability)
